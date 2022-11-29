c590d0c844fb57e9d887d95b14cd919bae6266ea
Benjamin <benjamin.bonnabaud@grenoble-inp.org>
2022-11-29 17:43:52 +0100

    unet update

--- UNet/train.py
+++ UNet/train.py
@@ -11,13 +11,16 @@ from torch import optim
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
-from utils.data_loading import BasicDataset, CarvanaDataset
+from utils.data_loading import BasicDataset
 from utils.dice_score import dice_loss
 from evaluate import evaluate
 from unet import UNet
 
-dir_img = Path('./data/imgs/')
-dir_mask = Path('./data/masks/')
+DATASET = ['Fluo-N2DL-HeLa','PhC-C2DH-U373']
+idx = 0
+
+dir_img = Path(f'../data/{DATASET[idx]}/IMG')
+dir_mask = Path(f'../data/{DATASET[idx]}/TARGET')
 dir_checkpoint = Path('./checkpoints/')
 
 
@@ -28,14 +31,10 @@ def train_net(net,
               learning_rate: float = 1e-5,
               val_percent: float = 0.1,
               save_checkpoint: bool = True,
-              img_scale: float = 0.5,
               amp: bool = False):
     # 1. Create dataset
-    try:
-        dataset = CarvanaDataset(dir_img, dir_mask, img_scale)
-    except (AssertionError, RuntimeError):
-        dataset = BasicDataset(dir_img, dir_mask, img_scale)
-
+    dataset = BasicDataset(dir_img, dir_mask)
+    
     # 2. Split into train / validation partitions
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
@@ -49,7 +48,7 @@ def train_net(net,
     # (Initialize logging)
     experiment = wandb.init(project='U-Net', resume='allow', anonymous='must')
     experiment.config.update(dict(epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
-                                  val_percent=val_percent, save_checkpoint=save_checkpoint, img_scale=img_scale,
+                                  val_percent=val_percent, save_checkpoint=save_checkpoint,
                                   amp=amp))
 
     logging.info(f'''Starting training:
@@ -60,7 +59,6 @@ def train_net(net,
         Validation size: {n_val}
         Checkpoints:     {save_checkpoint}
         Device:          {device.type}
-        Images scaling:  {img_scale}
         Mixed Precision: {amp}
     ''')
 
@@ -152,12 +150,11 @@ def get_args():
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
-    parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
     parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
                         help='Percent of the data that is used as validation (0-100)')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
-    parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
+    parser.add_argument('--classes', '-c', type=int, default=1, help='Number of classes')
 
     return parser.parse_args()
 
@@ -172,7 +169,7 @@ if __name__ == '__main__':
     # Change here to adapt to your data
     # n_channels=3 for RGB images
     # n_classes is the number of probabilities you want to get per pixel
-    net = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
+    net = UNet(n_channels=1, n_classes=args.classes, bilinear=args.bilinear)
 
     logging.info(f'Network:\n'
                  f'\t{net.n_channels} input channels\n'
@@ -190,7 +187,6 @@ if __name__ == '__main__':
                   batch_size=args.batch_size,
                   learning_rate=args.lr,
                   device=device,
-                  img_scale=args.scale,
                   val_percent=args.val / 100,
                   amp=args.amp)
     except KeyboardInterrupt:
--- UNet/utils/data_loading.py
+++ UNet/utils/data_loading.py
@@ -5,76 +5,66 @@ from pathlib import Path
 
 import numpy as np
 import torch
-from PIL import Image
+import skimage.io as skio
+import skimage
 from torch.utils.data import Dataset
 
 
 class BasicDataset(Dataset):
-    def __init__(self, images_dir: str, masks_dir: str, scale: float = 1.0, mask_suffix: str = ''):
+    def __init__(self, images_dir: str, masks_dir: str):
         self.images_dir = Path(images_dir)
         self.masks_dir = Path(masks_dir)
-        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
-        self.scale = scale
-        self.mask_suffix = mask_suffix
 
-        self.ids = [splitext(file)[0] for file in listdir(images_dir) if not file.startswith('.')]
-        if not self.ids:
+        self.img_ids = [splitext(file)[0] for file in listdir(images_dir) if not file.startswith('.')]
+        self.mask_ids = [splitext(file)[0] for file in listdir(masks_dir) if not file.startswith('.')]
+        if not self.img_ids:
             raise RuntimeError(f'No input file found in {images_dir}, make sure you put your images there')
-        logging.info(f'Creating dataset with {len(self.ids)} examples')
+        if len(self.mask_ids) != len(self.img_ids):
+            raise RuntimeError(f'Wrong number of masks in {masks_dir}, make sure you put your masks there')
+        logging.info(f'Creating dataset with {len(self.img_ids)} examples')
 
     def __len__(self):
-        return len(self.ids)
+        return len(self.img_ids)
 
     @staticmethod
-    def preprocess(pil_img, scale, is_mask):
-        w, h = pil_img.size
-        newW, newH = int(scale * w), int(scale * h)
-        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
-        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
-        img_ndarray = np.asarray(pil_img)
+    def preprocess(img, is_mask):
+        if is_mask :
+            img_ndarray = np.asarray(img > 0)
 
-        if not is_mask:
+        else :
+            img_float = skimage.img_as_float(img)
+            img_norm = (img_float -img_float.min())/(img_float.max() - img_float.min() + 1e-6)
+            img_ndarray = np.asarray(img_norm)
+
             if img_ndarray.ndim == 2:
                 img_ndarray = img_ndarray[np.newaxis, ...]
             else:
                 img_ndarray = img_ndarray.transpose((2, 0, 1))
 
-            img_ndarray = img_ndarray / 255
-
         return img_ndarray
 
     @staticmethod
     def load(filename):
-        ext = splitext(filename)[1]
-        if ext == '.npy':
-            return Image.fromarray(np.load(filename))
-        elif ext in ['.pt', '.pth']:
-            return Image.fromarray(torch.load(filename).numpy())
-        else:
-            return Image.open(filename)
+        return skio.imread(filename,plugin='pil')
 
     def __getitem__(self, idx):
-        name = self.ids[idx]
-        mask_file = list(self.masks_dir.glob(name + self.mask_suffix + '.*'))
-        img_file = list(self.images_dir.glob(name + '.*'))
+        name_img = self.img_ids[idx]
+        name_mask = self.mask_ids[idx]
+        mask_file = list(self.masks_dir.glob(name_mask + '.tif'))
+        img_file = list(self.images_dir.glob(name_img + '.tif'))
 
-        assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
-        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
+        assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name_img}: {img_file}'
+        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name_mask}: {mask_file}'
         mask = self.load(mask_file[0])
         img = self.load(img_file[0])
 
         assert img.size == mask.size, \
-            f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
+            f'Image and mask {name_img} & {name_mask} should be the same size, but are {img.size} and {mask.size}'
 
-        img = self.preprocess(img, self.scale, is_mask=False)
-        mask = self.preprocess(mask, self.scale, is_mask=True)
+        img = self.preprocess(img, is_mask=False)
+        mask = self.preprocess(mask, is_mask=True)
 
         return {
             'image': torch.as_tensor(img.copy()).float().contiguous(),
             'mask': torch.as_tensor(mask.copy()).long().contiguous()
         }
-
-
-class CarvanaDataset(BasicDataset):
-    def __init__(self, images_dir, masks_dir, scale=1):
-        super().__init__(images_dir, masks_dir, scale, mask_suffix='_mask')
