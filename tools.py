67f46859bac18513825c40de93ed5f1efa758ab0
Benjamin <benjamin.bonnabaud@grenoble-inp.org>
2022-12-06 11:38:53 +0100

    random crop

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import skimage.io as skio
 import skimage
+import numpy as np
 
 def plot_pred_with_target(target, seg, score):
     fig, ax = plt.subplots(2,2,figsize=(15,10))
@@ -25,3 +26,45 @@ def load_img_tg(IM_PATH, TG_PATH):
         img.append(im)
         tg.append(skio.imread(tg_path,plugin='pil') > 0)
     return img, tg
+
+def crop_img_tg(list_IM, list_TG, ratio):
+    divisors = common_divisors(list_IM[0].shape[0],list_IM[0].shape[1])
+    if ratio not in divisors :
+        raise ValueError(f"Please use a ratio that divide height and width. Available ratio : {divisors}")
+
+    X_train_cropped, y_train_cropped = [], []
+    #dimension of patchs = M*N
+    M = list_IM[0].shape[0]//ratio
+    N = list_IM[0].shape[1]//ratio
+    print(f'Initial dimensions of images : ({list_IM[0].shape[0]},{list_IM[0].shape[1]})')
+    print(f'Patch dimensions : ({M},{N})')
+
+    for k in range(len(list_IM)):
+        im = list_IM[k]
+        tg = list_TG[k]
+        im_tiles = [im[x:x+M, y:y+N] for x in range(0, im.shape[0], M) for y in range(0, im.shape[1], N)]
+        tg_tiles = [tg[x:x+M, y:y+N] for x in range(0, im.shape[0], M) for y in range(0, im.shape[1], N)]
+        scores = []
+
+        for i in range(len(im_tiles)):
+            scores.append(np.sum(tg_tiles[i]))
+
+        idx_best_score = np.argmax(scores)
+        X_train_cropped.append(im_tiles[idx_best_score])
+        y_train_cropped.append(tg_tiles[idx_best_score])
+
+    return X_train_cropped, y_train_cropped
+
+def common_divisors(num1, num2):
+    divs1 = []
+    for x in range (1, num1+1):
+        if (num1 % x) == 0:
+            divs1.append(x)
+    
+    divs = []
+    for div in divs1 :
+        if (num2 % div) == 0:
+            divs.append(div)
+
+    return divs
+    
